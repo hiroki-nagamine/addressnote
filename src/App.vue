6f504f2969089920e0519e83da5dbdc97fac6881
Hiroki Nagamine <nagaminehiroki@hnimac.local>
2019-08-26 19:08:15 +0900

    Address note init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+    <v-toolbar-side-icon @click="toggleSideMenu"></v-toolbar-side-icon>
+      <v-toolbar-title class="headline text-uppercase">
+      <span>マイアドレス帳</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+
+    </v-toolbar>
+    <SideNav/>
+    <v-content>
+    <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+import SideNav from './components/SideNav'
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'App',
+  components: {
+  SideNav
+},
+
+  data () {
+    return {
+      //
+    }
+  },
+  methods: {
+   ...mapActions(['toggleSideMenu'])
+  }
+}
+</script>
